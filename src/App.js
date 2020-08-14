da0b4639d33fd19ec5e1936d19333803fc169a07
anodeedge <smcurtis@gmail.com>
2020-08-13 21:35:43 -0700

    Added WPM and Word Display Functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { useState } from 'react'
-import lorem from './lorem'
-import './App.css';
+//Add 
+
+import React, { useState } from "react";
+import lorem from "./lorem";
+import "./App.css";
 
 function App() {
-  const [textVal, setTextVal] = useState(lorem.text)
-  const [speedVal, setSpeedVal] = useState(null)
+  const [textVal, setTextVal] = useState(lorem.text);
+  const [speedVal, setSpeedVal] = useState(1);
+  const [wordVal, setWordVal] = useState("----------");
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    const wpmForm = document.getElementById('wpm_form')
-    setSpeedVal(wpmForm.elements[0].value)
-  }
+  const calcWPM = () => {};
+  const displayWord = (wordList) => {
+    let i = 0;
+    setInterval(() => {
+      setWordVal(wordList[i]);
+      i++;
+    }, Math.floor(60000 / speedVal));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const wpmForm = document.getElementById("wpm_form");
+    var wordList = textVal.split(" ");
+    displayWord(wordList);
+  };
 
   return (
     <div className="App">
@@ -18,40 +31,45 @@ function App() {
         <h1>Word Reader</h1>
         <textarea
           placeholder="Paste or Write the text to be read."
-          style={{ fontSize: '24px' }}
+          style={{ fontSize: "24px" }}
           rows="8"
           cols="100"
-          value={lorem}
-          onChange={evt => setTextVal(evt.target.value)}
+          onChange={(evt) => {
+            setTextVal(evt.target.value);
+          }}
         />
-        <div style={{ margin: '10px 0' }}>Speed of Word Reader (WPM)</div>
+        <div style={{ margin: "10px 0" }}>Speed of Word Reader (WPM)</div>
         <form id="wpm_form" onSubmit={handleSubmit}>
           <input
             id="wpm"
-            min="0"
+            min="1"
             max="600"
+            form="wpm_form"
             name="wpm"
-            placeholder="0-600"
+            placeholder="1-600"
             required
-            style={{ fontSize: '24px' }}
+            style={{ fontSize: "24px" }}
             type="number"
+            onChange={(evt) =>{
+              setSpeedVal(evt.target.value);
+            }}
           />
-          <button id="submit" type="submit" style={{ margin: '0 0 0 10px', fontSize: '24px' }}>Submit</button>
+          <button
+            id="submit"
+            type="submit"
+            style={{ margin: "0 0 0 10px", fontSize: "24px" }}
+          >
+            Submit
+          </button>
         </form>
-        {`speed :${speedVal}`}
-        <p>
-        </p>
-        {/* {textVal} */}
-        {/* {{speedVal}} */}
-        {/* {lorem} */}
+        <div>{`Speed: ${speedVal}`}</div>
+        <div></div>
+        <div>{wordVal}</div>
       </header>
     </div>
-    
   );
 }
-
 export default App;
-
 
 // function Other(props) {
 //   const [counterVal, setCounterVal] = useState(0)
